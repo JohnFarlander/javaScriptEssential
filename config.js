934839b89944c5fbbe4efbaee6ec1aeb7d6e394d
JohnFarlander <omkar.purav@gmail.com>
2025-07-07 23:53:50 +0530

    re

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    OPENCAGE_API_KEY: 'YOUR_OPENCAGE_API_KEY', // Get from https://opencagedata.com/
-    TIMEZONEDB_API_KEY: 'YOUR_TIMEZONEDB_API_KEY', // Get from https://timezonedb.com/
-    UNSPLASH_API_KEY: 'YOUR_UNSPLASH_API_KEY' // Get from https://unsplash.com/developers
-}; 
