47944947d5b0ea503b3b35239eb2a92d6e23b2cc
JohnFarlander <omkar.purav@gmail.com>
2025-07-07 23:34:12 +0530

    tp

--- JS Project/config.sample.js
+++ JS Project/config.sample.js
@@ -2,11 +2,11 @@
 // Rename this file to config.js and add your API keys
 const config = {
     // Get from https://opencagedata.com/
-    OPENCAGE_API_KEY: 'your_opencage_api_key_here',
+    OPENCAGE_API_KEY: '4510ad066c89455e9e4e2b1a2bb4fc6a',
     
     // Get from https://timezonedb.com/
-    TIMEZONEDB_API_KEY: 'your_timezonedb_api_key_here',
+    TIMEZONEDB_API_KEY: 'SP84H24XZHOQ',
     
     // Get from https://unsplash.com/developers
-    UNSPLASH_API_KEY: 'your_unsplash_api_key_here'
+    UNSPLASH_API_KEY: '_cnx4B2hEHEf0d9Qz2T2xlc9wvYLo07uPiir3D1z5OM'
 }; 
